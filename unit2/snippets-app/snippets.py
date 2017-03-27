dc2d28a24e8e327c023c6d4beaa48f7fbf27cea8
Koji Yoshida <kojiyoshidastl@gmail.com>
2017-03-27 06:19:54 +0000

    add main - argparse

--- unit2/snippets-app/snippets.py
+++ unit2/snippets-app/snippets.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 # Set the log output file, and the log level
@@ -42,3 +43,11 @@ def delete(name):
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return "not deleted"
 
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
