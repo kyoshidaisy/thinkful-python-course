f96088f292c8d9bfdd69494b702ef9a2a4dc0034
Koji Yoshida <fortunoky@gmail.com>
2017-04-14 01:27:02 -0600

    bicycle add purchase dialog main.py l.67 error

--- unit1/Bicycle_Industry/main.py
+++ unit1/Bicycle_Industry/main.py
@@ -12,7 +12,7 @@ Aluroad = Bicycle("Aluroad", 10, 600)
 Carbonracer = Bicycle("Carbonracer", 7, 800)
 
 # shop data#
-pythonCyclery = Bikeshop("PythonCyclery",
+PythonCyclery = Bikeshop("PythonCyclery",
                          {
                              Cruiser: 5,
                              BMX: 7,
@@ -31,44 +31,44 @@ Matt = Customer("Matt", 1000)
 customers = [Dave, Paul, Matt]
 bike_purchases = {}
 
-print("\nWelcome our bike shop {}!\n".format(pythonCyclery.shopname))
+print("\nWelcome our bike shop {}!\n".format(PythonCyclery.shopname))
 
 # print1
 # name of the customer (each) and list of bikes in their budget
 bikelist = []
 for customer in customers:
-    print("{} Current Inventory for {}".format(pythonCyclery.shopname, customer.custname))
-    # pythonCyclery.show_inventory(customer.budget)
-    bikelist.extend(pythonCyclery.show_inventory(customer.budget))
+    print("{} Current Inventory for {}".format(PythonCyclery.shopname, customer.custname))
+    # print(PythonCyclery.show_inventory(9999))
+    PythonCyclery.show_inventory(customer.budget)
+    print('\n\n')
+    bikelist.extend(PythonCyclery.show_inventory(customer.budget))
     print(bikelist)
     # bike purchase dialog -- ask customer which bike he wants from the list and return bike_purchases
-    bike_to_buy = input("\n{}, Which bike are you going to buy?   :".format(customer.custname))
+    bike_to_buy = str(input("\n\n{}, Which bike are you going to buy?   :".format(customer.custname)))
     if bike_to_buy in bikelist:
-        bike_purchases[customer.custname] = bike_to_buy  # Why the attributes come with ''? How to remove it?
-        bikelist = []
-        print('\n\n')
+        bike_purchases[customer.custname] = bike_to_buy  # Why it becomes as string with ' '? how to fix it?
     else:
         print('please select a bike from the list')
 
 # print2
 # initial inventory of the shop
-print("Shop: {} -- Initial Inventory".format(pythonCyclery.shopname))
-pythonCyclery.show_inventory(9999)
+print("Shop: {} -- Initial Inventory".format(PythonCyclery.shopname))
+PythonCyclery.show_inventory(9999)
 print('\n\n')
 
 # print3
 # have each of the customer purchase a bike
 # print the name of bike, cost, rest of his fund
-pythonCyclery.reset_profit()
-#bike_purchases = {Dave: Cruiser, Paul: Hardtail, Matt: Carbonracer, }
+PythonCyclery.reset_profit()
+# bike_purchases = {Dave: Cruiser, Paul: Hardtail, Matt: Carbonracer, }
 print(bike_purchases)
 for customer in bike_purchases:
     bike_sold = bike_purchases[customer]
     customer.purchase(bike_sold)
-    pythonCyclery.sell_bike(bike_sold)
+    PythonCyclery.sell_bike(bike_sold)
     print("  ")
 
 # after each purchase, update inventory
-print("{} Updated inventory".format(pythonCyclery.shopname))
-pythonCyclery.show_inventory(9999)
-print("   {} Total Profit is ${}".format(pythonCyclery.shopname, pythonCyclery.profit))
+print("{} Updated inventory".format(PythonCyclery.shopname))
+PythonCyclery.show_inventory(9999)
+print("   {} Total Profit is ${}".format(PythonCyclery.shopname, PythonCyclery.profit))
