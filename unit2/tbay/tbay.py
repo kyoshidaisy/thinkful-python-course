812afa52b9d18f6934418d346a472f77852a80f5
Koji Yoshida <fortunoky@gmail.com>
2017-04-25 06:06:45 -0600

    completed tbay project

--- unit2/tbay/tbay.py
+++ unit2/tbay/tbay.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,8 +9,8 @@ Base = declarative_base()
 
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, Float
-
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 class Item(Base):
     __tablename__ = "items"
@@ -20,6 +20,9 @@ class Item(Base):
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
 
+    auctioneer_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    item_bid = relationship("Bid", backref="item")
+
 
 class User(Base):
     __tablename__ = "users"
@@ -28,6 +31,8 @@ class User(Base):
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
 
+    auctions = relationship("Item", backref="auctioneer")
+    bids = relationship("Bid", backref="bidder")
 
 class Bid(Base):
     __tablename__ = "bid"
@@ -35,5 +40,33 @@ class Bid(Base):
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
 
+    bidder_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+
 
 Base.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    aimee = User(username="anoble", password="l8blmr")
+    melissa = User(username="mherrera", password="missylou")
+    melinda = User(username="maragon", password="arcadia")
+    session.add_all([aimee, melissa, melinda])
+    session.commit()
+
+    baseball = Item(name="baseball", description="toy", auctioneer=aimee)
+    session.add(baseball)
+    session.commit()
+
+    melissa_bid1 = Bid(price=50, item=baseball, bidder=melissa)
+    melinda_bid1 = Bid(price=55, item=baseball, bidder=melinda)
+    melissa_bid2 = Bid(price=61, item=baseball, bidder=melissa)
+    melinda_bid2 = Bid(price=72.1, item=baseball, bidder=melinda)
+    session.add_all([melissa_bid1, melinda_bid1, melinda_bid2, melissa_bid2])
+    session.commit()
+
+    highest_bid = session.query(Bid).order_by(desc(Bid.price)).first()
+    winner = highest_bid.bidder.username
+    won_price = highest_bid.price
+
+    print("The winner is: ", winner)
+    print("Final price is ", won_price, "dollar.")
